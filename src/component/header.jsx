f2d3212f4778320e81e3056f38fd6e2ee91a2579
jayantdeepak <jayantdeepak49@gmail.com>
2024-05-20 03:29:17 +0530

    moduler componets

---
+++ src/component/header.jsx
@@ -0,0 +1,16 @@
+export default function Header (){
+    return(<div className='header'>
+  <div className='logo'>
+  <img src='https://img.freepik.com/premium-vector/chef-food-restaurant-logo_7085-179.jpg'/>
+  </div>
+  <div className='navigation-div'><ul>
+    <li>about us</li>
+    <li>contact us</li>
+    <li>cart</li>
+    </ul>
+  </div>
+  
+    </div>
+  
+    )
+  }
---
+++ src/component/restcard.jsx
@@ -0,0 +1,17 @@
+export default function Restcard({details}){
+    
+    const{info:{name,cloudinaryImageId,id,cuisines,avgRating}}=details
+
+  
+  return(
+    <div className='rest-card'>
+      <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/"+ cloudinaryImageId}></img>
+      <h3>{name}</h3>
+      <h5>{cuisines.join(",")}</h5>
+      <h3>{avgRating}</h3>
+      
+      <h4>north indian, soutindian</h4>
+
+    </div>
+  )
+}
